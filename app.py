92da298e7ddc30a5d62e15348200c6b89a7dbf76
root <root@ip-10-0-143-106.ap-northeast-2.compute.internal>
2024-10-02 06:26:51 +0000

    20240102.WED | fixed app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Cloud! - CI/CD_Pipeline'
+    return '<h1>Hello Cloud Native! - CI/CD_Pipeline <p>&#9829;</p></h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
